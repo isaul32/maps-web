7606e31a0096bc5b549c3af2de206e5df178f3a5
Sauli Anto <sauli.anto@gmail.com>
2024-07-20 17:54:34 +0300

    Update datasource

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,9 +2,7 @@ import { FC } from "react";
 import StaticMap from "react-map-gl";
 import maplibregl from "maplibre-gl";
 import DeckGL from "@deck.gl/react";
-import { LineLayer } from "@deck.gl/layers";
 import { MapContext } from "react-map-gl/dist/esm/components/map";
-import { Layer } from "deck.gl";
 
 const INITIAL_VIEW_STATE = {
   longitude: 23.78712,
@@ -13,14 +11,6 @@ const INITIAL_VIEW_STATE = {
   pitch: 0,
   bearing: 0,
 };
-
-const data = [
-  {
-    sourcePosition: [23.78712, 61.52],
-    targetPosition: [23.78, 61.519],
-  },
-];
-
 const Map: FC = () => {
   const controller = {
     // scrollZoom: {
@@ -30,15 +20,11 @@ const Map: FC = () => {
     keyboard: false,
     inertia: true,
   };
-  const layers: Layer<unknown>[] = [
-    // new LineLayer({ id: "line-layer", data })
-  ];
 
   return (
     <DeckGL
       initialViewState={INITIAL_VIEW_STATE}
       controller={controller}
-      layers={layers}
       ContextProvider={MapContext.Provider}
     >
       <StaticMap mapLib={maplibregl} mapStyle="style.json" />
